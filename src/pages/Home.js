1e64c2a8d6920c145665609b594d742229ad49e8
Ryan Foster-Hill <rfoster-hill@hotmail.com>
2023-10-12 13:14:54 +0100

    changed galleries to carousels

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,16 @@
 import React from "react"
+import Carousel from "react-bootstrap/Carousel"
+import "bootstrap/dist/css/bootstrap.min.css"
 
 import '../styling/Home.css'
 
 import img1 from "../resources/work_images/340078528_3465254913717183_2014099130808656391_n.jpg"
 import img2 from "../resources/work_images/340236116_2544627039025523_5350044887685715958_n.jpg"
 
-import img3 from "../resources/work_images/322179521_612408750648479_9055949165633037179_n.jpg"
-import img4 from "../resources/work_images/322383275_760025312133043_3309659167942817247_n.jpg"
+// import img3 from "../resources/work_images/322179521_612408750648479_9055949165633037179_n.jpg"
+// import img4 from "../resources/work_images/322383275_760025312133043_3309659167942817247_n.jpg"
 
-import img5 from "../resources/work_images/311112602_177119294838039_7770649718407190026_n.jpg"
+// import img5 from "../resources/work_images/311112602_177119294838039_7770649718407190026_n.jpg"
 
 
 import img6 from "../resources/work_images/286898252_151066560776646_3999805167406394404_n.jpg"
@@ -21,31 +23,77 @@ import img9 from "../resources/work_images/278759728_141163605100275_30085262362
 const Home = () => {
     return (
         <div id="homeContainer">
+            <Carousel interval={null} variant="dark" className="carouselHome">
+
+                <Carousel.Item>
+                    <div className="carouselWrap">
+                        <div className="carouselImages">
+                            <img className="carouselImage" src={img2} alt="img2" />
+                            <img className="carouselImage" src={img1} alt="img1" />
+                        </div>
+                    </div>
+
+
+                </Carousel.Item>
+
+                <Carousel.Item>
+                    <div className="carouselWrap">
+                        <div className="carouselImages">
+                            <img className="carouselImage" src={img6} alt="img6" />
+                            <img className="carouselImage" src={img7} alt="img7" />
+                        </div>
+                    </div>
+
+
+                </Carousel.Item>
+
+                <Carousel.Item>
+                    <div className="carouselWrap">
+                        <div className="carouselImages">
+                            <img className="carouselImage" src={img8} alt="img8" />
+                            <img className="carouselImage" src={img9} alt="img9" />
+                        </div>
+                    </div>
+
+
+                </Carousel.Item>
+
+                {/* <Carousel.Item>
+                    <div className="carouselWrap">
+                        <div className="carouselImages">
+                            <img className="carouselImage" src={img5} alt="img5" />
+                        </div>
+                    </div>
+
+
+                </Carousel.Item> */}
+            </Carousel>
+
             <div id="homeGalleryCont">
+                {/* 
+                <div id="homeGalleryImgWrap">
+                    <img className="homeGalleryImg" src={img2} alt="img2"/>
+                    <img className="homeGalleryImg" src={img1} alt="img1"/>
+                </div>
 
-            <div id="homeGalleryImgWrap">
-                <img className="homeGalleryImg" src={img2} alt="img2"/>
-                <img className="homeGalleryImg" src={img1} alt="img1"/>
-            </div>
+                <div id="homeGalleryImgWrap">
+                    <img className="homeGalleryImg" src={img6} alt="img6"/>
+                    <img className="homeGalleryImg" src={img7} alt="img7"/>
+                </div>
 
-            <div id="homeGalleryImgWrap">
-                <img className="homeGalleryImg" src={img6} alt="img6"/>
-                <img className="homeGalleryImg" src={img7} alt="img7"/>
-            </div>
+                <div id="homeGalleryImgWrap">
+                    <img className="homeGalleryImg" src={img8} alt="img8"/>
+                    <img className="homeGalleryImg" src={img9} alt="img9"/>
+                </div>
 
-            <div id="homeGalleryImgWrap">
-                <img className="homeGalleryImg" src={img8} alt="img8"/>
-                <img className="homeGalleryImg" src={img9} alt="img9"/>
-            </div>
+                <div id="homeGalleryImgWrapTall">
+                    <img className="homeGalleryImg homeGalleryImgTall" src={img4} alt="img4"/>
+                    <img className="homeGalleryImg homeGalleryImgTall" src={img3} alt="img3"/>
+                </div>
 
-            <div id="homeGalleryImgWrapTall">
-                <img className="homeGalleryImg homeGalleryImgTall" src={img4} alt="img4"/>
-                <img className="homeGalleryImg homeGalleryImgTall" src={img3} alt="img3"/>
-            </div>
-
-            <div id="homeGalleryImgWrap">
-                <img className="homeGalleryImg" src={img5} alt="img5"/>
-            </div>
+                <div id="homeGalleryImgWrap">
+                    <img className="homeGalleryImg" src={img5} alt="img5"/>
+                </div> */}
 
             </div>
 
--- src/pages/Reviews.js
+++ src/pages/Reviews.js
@@ -1,43 +1,77 @@
-//MAKE IT SO SCROLLBAR RESETS ON COMPONENT LOAD (MAYBE USING scrollLeft = 0) https://stackoverflow.com/questions/47280054/reset-scroll-position-on-component-re-render
-
 import '../styling/Reviews.css'
+import Carousel from "react-bootstrap/Carousel"
+import "bootstrap/dist/css/bootstrap.min.css"
 
 const Reviews = () => {
 
-    window.scrollTo(0,0)
+    window.scrollTo(0, 0)
 
     return (
         <div id="reviewsWrapper">
 
-            <div className='reviewContainer'>
-                <p className='reviewContent'>"Very good decorators, friendy, nothings too much trouble, mother & daughter team. Very reasonable prices, double the quickness being a pair. Highly recommended 😊"</p>
-                <p className='reviewName'>Lynn James</p>
-            </div>
+            <Carousel interval={null} variant='dark' className='carouselReviews'>
+                <Carousel.Item>
+                    <div className='reviewWrap'>
+                        <div className='reviewContainer'>
+                            <p className='reviewContent'>"Very good decorators, friendy, nothings too much trouble, mother & daughter team. Very reasonable prices, double the quickness being a pair. Highly recommended 😊"</p>
+                            <p className='reviewName'>Lynn James</p>
+                        </div>
+                    </div>
 
-            <div className='reviewContainer'>
-                <p className='reviewContent'>"You have both done the most amazing job transforming what was a very run down shabby and neglected bungalow into my beautiful new home, you've worked so hard and I can't thank you enough!"</p>
-                <p className='reviewName'>Anne Shaw</p>
-            </div>
+                </Carousel.Item>
 
-            <div className='reviewContainer'>
-                <p className='reviewContent'>"Excellent decorators have carried out work for us on two occasions. Punctual, great communication and a high standard of work. Would highly recommend"</p>
-                <p className='reviewName'>Ginge Davies</p>
-            </div>
+                <Carousel.Item>
+                    <div className='reviewWrap'>
+                        <div className='reviewContainer'>
+                            <p className='reviewContent'>"You have both done the most amazing job transforming what was a very run down shabby and neglected bungalow into my beautiful new home, you've worked so hard and I can't thank you enough!"</p>
+                            <p className='reviewName'>Anne Shaw</p>
+                        </div>
+                    </div>
 
-            <div className='reviewContainer'>
-                <p className='reviewContent'>"Thanks to Sandy & Eli for painting full downstairs, very efficient and reasonably priced. Highly recommend if your looking for someone to come in and just get the job done without any hassle"</p>
-                <p className='reviewName'>Ash Seed</p>
-            </div>
+                </Carousel.Item>
 
-            <div className='reviewContainer'>
-                <p className='reviewContent'>"Thanks Sandy and Eli, great job, really pleased with how it turned out"</p>
-                <p className='reviewName'>Patricia Schofield</p>
-            </div>
+                <Carousel.Item>
+                    <div className='reviewWrap'>
+                        <div className='reviewContainer'>
+                            <p className='reviewContent'>"Excellent decorators have carried out work for us on two occasions. Punctual, great communication and a high standard of work. Would highly recommend"</p>
+                            <p className='reviewName'>Ginge Davies</p>
+                        </div>
+                    </div>
 
-            <div className='reviewContainer'>
-                <p className='reviewContent'>"Thanks both, great job as always x"</p>
-                <p className='reviewName'>Samantha Jane</p>
-            </div>
+                </Carousel.Item>
+
+                <Carousel.Item>
+
+                    <div className='reviewWrap'>
+                        <div className='reviewContainer'>
+                            <p className='reviewContent'>"Thanks to Sandy & Eli for painting full downstairs, very efficient and reasonably priced. Highly recommend if your looking for someone to come in and just get the job done without any hassle"</p>
+                            <p className='reviewName'>Ash Seed</p>
+                        </div>
+                    </div>
+
+                </Carousel.Item>
+
+                <Carousel.Item>
+                    <div className='reviewWrap'>
+                        <div className='reviewContainer'>
+                            <p className='reviewContent'>"Thanks Sandy and Eli, great job, really pleased with how it turned out"</p>
+                            <p className='reviewName'>Patricia Schofield</p>
+                        </div>
+                    </div>
+
+                </Carousel.Item>
+
+                <Carousel.Item>
+                    <div className='reviewWrap'>
+                        <div className='reviewContainer'>
+                            <p className='reviewContent'>"Thanks both, great job as always x"</p>
+                            <p className='reviewName'>Samantha Jane</p>
+                        </div>
+                    </div>
+
+                </Carousel.Item>
+
+            </Carousel>
 
         </div>
     )
